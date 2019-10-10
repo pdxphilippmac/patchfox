77d75059066bc13d500888bcd865497cd50588e2
Philipp Maecke <p.maecke@gmx.de>
2019-10-10 15:40:15 +0200

    add globalstyle.js with mock datza

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import GlobalStyles from "./GlobalStyles";
+import Footer from "./components/Footer";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <GlobalStyles />
+      <>
+        <Footer></Footer>
+      </>
+    </div>
+  );
 }
 
 export default App;
