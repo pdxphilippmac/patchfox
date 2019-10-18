6cab45efb630bbc9d07d0dbd8e5debea4b2d64e5
Philipp Maecke <p.maecke@gmx.de>
2019-10-18 19:56:36 +0200

    display one game on page via fetch

--- src/components/DataFetch.js
+++ src/components/DataFetch.js
@@ -1,5 +1,19 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import styled from "styled-components";
+
+const SearchItem = styled.article`
+  display: flex;
+  flex-direction: column;
+  background-color: #3b3434e6;
+  margin: 5px;
+  color: white;
+  padding: 10px;
+  border-radius: 4px;
+  font-family: "futura";
+
+  border: #707070e6 solid 0.5px;
+`;
 
 export default function DataFetch() {
   const [posts, setPosts] = useState([]);
@@ -9,14 +23,15 @@ export default function DataFetch() {
 
   useEffect(() => {
     const proxyUrl = "https://cors-anywhere.herokuapp.com/";
-    const targetUrl = "https://api-v3.igdb.com/characters/";
+    const targetUrl =
+      "https://api-v3.igdb.com/games/1026?fields=id,name,url,summary,storyline,websites";
     axios
       .get(proxyUrl + targetUrl, {
         headers: {
           "user-key": "e2715f17601c1d968b592f747c6aa839",
           Accept: "application/json"
         },
-        data: "fields akas,country_name,created_at,description,games;"
+        data: "fields category,description,position,title,values;"
       })
       .then(res => {
         console.log(res);
@@ -28,11 +43,13 @@ export default function DataFetch() {
   }, []);
   return (
     <div>
-      <ul>
-        {posts.map(post => (
-          <li key={post.id}>{post.id}</li>
-        ))}
-      </ul>
+      {posts.map(post => (
+        <SearchItem key={post.id}>
+          <p>{post.name}</p>
+          <p>{post.storyline}</p>
+          <p>{post.summary} </p>
+        </SearchItem>
+      ))}
     </div>
   );
 }
