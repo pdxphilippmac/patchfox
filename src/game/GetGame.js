83d173742a0ca2b705902a36ddaa4d00489916df
Philipp Maecke <p.maecke@gmx.de>
2019-11-06 10:13:42 +0100

    add add function to news page

--- src/game/GetGame.js
+++ src/game/GetGame.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Fade from "react-reveal/Fade";
-
+import AddArrow from "../icons/addArrow";
+import addToJsonDb from "../api/addToJsonDb";
 import styled from "styled-components";
 import { useHistory, useParams } from "react-router-dom";
 import GlobalStyles from "../GlobalStyles";
 import NeonGlow from "../components/AlterNeonGlow";
 import NeonGlowLoading from "../components/NeonGlowLoading";
 import HomeIcon from "../icons/footerHome";
+import PlusIcon from "../icons/footerPlus";
 
 const TestDiv = styled.article`
   background: #262122e6;
@@ -35,22 +37,16 @@ const CoverImg = styled.img`
   max-width: 300px;
   margin: 35px;
 `;
+function handleClick(name, id, cover) {
+  addToJsonDb(name, id, cover);
+  console.log(name);
+}
 
 export default function GetGame({ info, match }) {
   const [game, setGame] = useState([]);
 
-  // const [params, setParams] = useState([]);
   const [loading, setLoading] = useState(true);
   console.log(`Info log ${info}`, match.params.gameId);
-  // return arrToObj(info);
-  // function arrayToObject(array) {
-  //   const gameObject = {};
-  //   for (let i = 0; i < array.length; i++) {
-  //     gameObject[array[i].id] = array[i];
-  //   }
-  //   return gameObject;
-
-  // }
 
   useEffect(() => {
     console.log(`Info log ${info}`);
@@ -95,13 +91,20 @@ export default function GetGame({ info, match }) {
           <TestDiv>
             {game.map(item => (
               <FlexDiv>
+                <button
+                  name={item.name}
+                  id={item.id}
+                  onClick={() => handleClick(item.name, item.id, item.cover)}
+                >
+                  <PlusIcon />
+                </button>
                 <CoverImg
                   alt="CoverImage "
                   src={item.cover.url.replace("t_thumb", "t_cover_big")}
                 />
 
                 <p>Release Date: {item.release_dates[0].human}</p>
-                {/* <p>Game-ID:{item.id}</p> */}
+                <p>Game-ID:{item.id}</p>
 
                 <h1>{item.name}</h1>
                 <p>
@@ -111,19 +114,11 @@ export default function GetGame({ info, match }) {
                 </p>
                 <p>{item.summary}</p>
                 <p>Game Version 3.2.5</p>
-                {/* 
-                <p>
+
+                {/* <p>
                   Companies: {item.involved_companies[0].company.name},
                   {item.involved_companies[1].company.name}
                 </p> */}
-
-                {/* <p>
-              {game.involved_companies.map(plat => (
-                <p>{plat.company.name}</p>
-              ))}
-            </p> */}
-
-                {/* <p>This is {item.name}</p> */}
               </FlexDiv>
             ))}
           </TestDiv>
